74790d6341daa377fd7a14a4abbafdf22918575d
Keith Tremorin <manwithnoname6@hotmail.com>
2016-01-03 15:36:09 -0600

    added ability to select system.

--- ScreenObjects/MainWindow.cs
+++ ScreenObjects/MainWindow.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 using TestStack.White;
 using TestStack.White.Factory;
+using TestStack.White.UIItems;
 using TestStack.White.UIItems.Finders;
+using TestStack.White.UIItems.ListBoxItems;
 using TestStack.White.UIItems.TabItems;
+using TestStack.White.UIItems.TableItems;
 using TestStack.White.UIItems.WindowItems;
+using TestStack.White.UIItems.WPFUIItems;
 using TestStack.White.Utility;
 
 
@@ -24,6 +27,32 @@ public enum MainTabs
     Games,
     Pause
 }
+
+public enum Systems
+{
+    Global,
+    Atari5200,
+    AtariLynx,
+    Daphne,
+    MAME,
+    NECTurboGrafx16,
+    N64,
+    NES,
+    GBA,
+    GameCube,
+    Panasonic3DO,
+    Sega32X,
+    SegaCD,
+    Dreamcast,
+    GameGear,
+    Genesis,
+    MasterSystem,
+    SegaModel2,
+    NeoGeo,
+    NeoGeoPocketColor,
+    Playstation,
+    SNES
+}
 namespace ScreenObjects
 {
     public class MainWindow
@@ -32,6 +61,8 @@ namespace ScreenObjects
         public MainWindow(TestStack.White.Application app)
         {
             Retry.For(() => root = app.GetWindow(SearchCriteria.ByAutomationId("RocketLauncherUI"), InitializeOption.WithCache), TimeSpan.FromSeconds(5));
+
+            root.LogStructure();
         }
 
 
@@ -75,7 +106,98 @@ namespace ScreenObjects
             return this;
         }
 
+        public MainWindow SetSystem(Systems system)
+        {
+            string sSystem = null;
 
+            switch (system)
+            {
+                case Systems.Atari5200:
+                    sSystem = "Atari 5200";
+                    break;
+                case Systems.AtariLynx:
+                    sSystem = "Atari Lynx";
+                    break;
+                case Systems.Daphne:
+                    sSystem = "Daphne";
+                    break;
+                case Systems.Dreamcast:
+                    sSystem = "Sega Dreamcast";
+                    break;
+                case Systems.GameCube:
+                    sSystem = "Nintendo GameCube";
+                    break;
+                case Systems.GameGear:
+                    sSystem = "Sega Game Gear";
+                    break;
+                case Systems.GBA:
+                    sSystem = "Nintendo Game Boy Advance";
+                    break;
+                case Systems.Genesis:
+                    sSystem = "Sega Genesis";
+                    break;
+                case Systems.Global:
+                    sSystem = "GLOBAL";
+                    break;
+                case Systems.MAME:
+                    sSystem = "MAME";
+                    break;
+                case Systems.MasterSystem:
+                    sSystem = "Sega Master System";
+                    break;
+                case Systems.N64:
+                    sSystem = "Nintendo 64";
+                    break;
+                case Systems.NECTurboGrafx16:
+                    sSystem = "NEC TurboGrafx-16";
+                    break;
+                case Systems.NeoGeo:
+                    sSystem = "SNK Neo Geo";
+                    break;
+                case Systems.NeoGeoPocketColor:
+                    sSystem = "SNK Neo Geo Pocket Color";
+                    break;
+                case Systems.NES:
+                    sSystem = "Nintendo Entertainment System";
+                    break;
+                case Systems.Panasonic3DO:
+                    sSystem = "Panasonic 3DO";
+                    break;
+                case Systems.Playstation:
+                    sSystem = "Sony PlayStation";
+                    break;
+                case Systems.Sega32X:
+                    sSystem = "Sega 32X";
+                    break;
+                case Systems.SegaCD:
+                    sSystem = "Sega CD";
+                    break;
+                case Systems.SegaModel2:
+                    sSystem = "Sega Model 2";
+                    break;
+                case Systems.SNES:
+                    sSystem = "Super Nintendo Entertainment System";
+                    break;
+            }
+
+            return SetSystem(sSystem);
+        }
+
+        public MainWindow SetSystem(string sSystem)
+        {
+            root.Get<ListView>().Select(sSystem);
+            return this;
+        }
+
+        public MainWindow SelectEachSystem()
+        {
+            for(int i=0; i <root.Get<ListView>().Rows.Count; i++)
+            {
+                root.Get<ListView>().Select(i);
+            }
+
+            return this;
+        }
 
 
     }
--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -10,7 +10,7 @@ namespace Tests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethod1()   
         {
             string path = @"E:\RocketLauncher\RocketLauncherUI";
 
@@ -24,7 +24,30 @@ namespace Tests
                 .SetMainTab(MainTabs.Emulators)
                 .SetMainTab(MainTabs.Modules)
                 .SetMainTab(MainTabs.Games)
-                .SetMainTab(MainTabs.Pause);
+                .SetMainTab(MainTabs.Pause)
+                .SelectEachSystem();
+                //.SetSystem(Systems.Atari5200)
+                //.SetSystem(Systems.AtariLynx)
+                //.SetSystem(Systems.Daphne)
+                //.SetSystem(Systems.Dreamcast)
+                //.SetSystem(Systems.GameCube)
+                //.SetSystem(Systems.GameGear)
+                //.SetSystem(Systems.GBA)
+                //.SetSystem(Systems.Genesis)
+                //.SetSystem(Systems.Global)
+                //.SetSystem(Systems.MAME)
+                //.SetSystem(Systems.MasterSystem)
+                //.SetSystem(Systems.N64)
+                //.SetSystem(Systems.NECTurboGrafx16)
+                //.SetSystem(Systems.NeoGeo)
+                //.SetSystem(Systems.NeoGeoPocketColor)
+                //.SetSystem(Systems.NES)
+                //.SetSystem(Systems.Panasonic3DO)
+                //.SetSystem(Systems.Playstation)
+                //.SetSystem(Systems.Sega32X)
+                //.SetSystem(Systems.SegaCD)
+                //.SetSystem(Systems.SegaModel2)
+                //.SetSystem(Systems.SNES);
 
 
 
