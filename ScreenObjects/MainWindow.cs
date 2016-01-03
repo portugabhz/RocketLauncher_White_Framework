dceb28c2a1b82b9dd40eecacd77ef056639ae8b9
Keith Tremorin <manwithnoname6@hotmail.com>
2016-01-03 11:47:42 -0600

    initial commit

---
+++ ScreenObjects/MainWindow.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TestStack.White;
+using TestStack.White.Factory;
+using TestStack.White.UIItems.Finders;
+using TestStack.White.UIItems.TabItems;
+using TestStack.White.UIItems.WindowItems;
+
+
+public enum MainTabs
+{
+    Keymapper,
+    LogViewer,
+    RocketLauncherUI,
+    GeneralSettings,
+    Settings,
+    Emulators,
+    Modules,
+    Games,
+    Pause
+}
+namespace ScreenObjects
+{
+    public class MainWindow
+    {
+        Window root;
+        public MainWindow(TestStack.White.Application app)
+        {
+            root = app
+                .GetWindow(SearchCriteria.ByAutomationId("RocketLauncherUI"), InitializeOption.WithCache);
+        }
+
+
+        public void SetMainTab(MainTabs tab)
+        {
+            string sTabAID = null;
+            switch(tab)
+            {
+                case MainTabs.Emulators:
+                    break;
+                case MainTabs.Games:
+                    sTabAID = "tabGames";
+                    break;
+                case MainTabs.GeneralSettings:
+                    break;
+                case MainTabs.Keymapper:
+                    break;
+                case MainTabs.LogViewer:
+                    break;
+                case MainTabs.Modules:
+                    break;
+                case MainTabs.Pause:
+                    break;
+                case MainTabs.RocketLauncherUI:
+                    sTabAID = "tabRLUI";
+                    break;
+                case MainTabs.Settings:
+                    sTabAID = "tabSettings";
+                    break;
+
+            }
+
+            root.Get<Tab>(SearchCriteria.ByAutomationId("tabMain")).Pages.Where(x => x.AutomationElement.Current.AutomationId.Equals(sTabAID)).Single().Click();
+        }
+
+    }
+}
---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestStack.White;
+using ScreenObjects;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"E:\RocketLauncher\RocketLauncherUI";
+
+            Application app = Application.Launch(path + @"\RocketLauncherUI.exe");
+
+            System.Threading.Thread.Sleep(5000);
+            new MainWindow(app).SetMainTab(MainTabs.Games);
+
+        }
+    }
+}
